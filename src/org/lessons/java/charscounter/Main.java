f28315e72f8aa71d9c5a1e187f119b6fb27a37f1
patrictobola <tobola92@gmail.com>
2023-11-17 14:32:14 +0100

    done exercise

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,10 +1,30 @@
 package org.lessons.java.charscounter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("Scegli una parola/frase da scomporre: ");
+		String value = in.nextLine();
+		
+		Map<Character, Integer> charCounter = new HashMap<>();
+		
+		for(Character c : value.toLowerCase().toCharArray()) {
+			if(charCounter.containsKey(c)) {
+				charCounter.put(c, charCounter.get(c) + 1);
+			} else {
+				charCounter.put(c, 1);
+			}
+		}
+		
+		System.out.println(charCounter);
+		
 	}
+
+	
 
 }
